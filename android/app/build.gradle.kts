bc2c41132e4c14f0d3a0e40078e41afb0d56dbd3
Patricia130 <147952863+Patricia130@users.noreply.github.com>
2025-11-05 15:15:26 -0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.tochegando.cliente"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
